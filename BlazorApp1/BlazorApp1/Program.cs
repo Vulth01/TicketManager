5fa4ed279463872dd3129687ab443f610df23406
Vulth01 <damienharwood14@gmail.com>
2025-03-12 12:11:52 +0200

    Added: Database Implementation || Updated: CRUD methods
    - Local database added, project will have to be updated in future for use of online database.
    
    - Create/Read/Update/Delete methods have been updated from local project implementation to local database.
    Online database implementation will remain the same as only the path will be changed.
    
    - Validation methods are not ideal, update required.

--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -1,7 +1,16 @@
 using BlazorApp1.Clients;
 using BlazorApp1.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BlazorApp1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<TicketContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TicketContext") ?? throw new InvalidOperationException("Connection string 'TicketContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -17,6 +26,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
